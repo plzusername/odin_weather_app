0b11693cc319ed9c76764ad1b6a9c037c7e60128
plzusername <MohamedFaris2210@gmail.com>
2024-04-07 11:07:25 +0300

    Append footer to body

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import generateHeader from "./Modules/generate-content/generateHeader";
 import generateMain from "./Modules/generate-content/generateMain";
+import generateFooter from "./Modules/generate-content/generateFooter";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 const header = generateHeader();
 const main = generateMain();
+const footer = generateFooter();
 
 document.body.appendChild(header);
 document.body.appendChild(main);
+document.body.appendChild(footer);
