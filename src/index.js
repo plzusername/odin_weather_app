1e56f36aca908a427befa3ebedd00f52d80c2059
plzusername <MohamedFaris2210@gmail.com>
2024-04-07 10:37:53 +0300

    Add main section to body

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import generateHeader from "./Modules/generate-content/generateHeader";
+import generateMain from "./Modules/generate-content/generateMain";
 
 const header = generateHeader();
+const main = generateMain();
 
 document.body.appendChild(header);
+document.body.appendChild(main);
